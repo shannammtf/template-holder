795e705406ecf564bdfa17aadf353cb26054c658
Shan Nam <gimmetinhnhua@Fleetwood-Mac-book.local>
2023-11-16 23:43:18 +0700

    add spring template

---
+++ template_spring_boot/${{value.java_artifact_name}}/src/main/java/com/shannammtf/backstagepoc/${{value.java_artifact_name}}Application.java
@@ -0,0 +1,13 @@
+package com.shannammtf.backstagepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ${{value.java_artifact_name}}Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(${{value.java_artifact_name}}Application.class, args);
+	}
+
+}
---
+++ template_spring_boot/${{value.java_artifact_name}}/src/test/java/com/shannammtf/backstagepoc/${{value.java_artifact_name}}ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shannammtf.backstagepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ${{value.java_artifact_name}}ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
